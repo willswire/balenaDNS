b7d93c1f4fb7d1dc3bcb9f9a52018f92f0ec2653
Will Walker <wwalker@udel.edu>
2020-03-22 16:00:05 -0400

    format documents

--- networking/start.sh
+++ networking/start.sh
@@ -22,31 +22,31 @@ echo -e "Setting DNS servers to:\n$DNS_1\n$DNS_2"
 delimiter=.
 
 ip_one=$DNS_1$delimiter
-ip_one_array=();
+ip_one_array=()
 while [[ $ip_one ]]; do
-    ip_one_array+=( "${ip_one%%"$delimiter"*}" );
-    ip_one=${ip_one#*"$delimiter"};
-done;
+      ip_one_array+=("${ip_one%%"$delimiter"*}")
+      ip_one=${ip_one#*"$delimiter"}
+done
 
 ip_two=$DNS_2$delimiter
-ip_two_array=();
+ip_two_array=()
 while [[ $ip_two ]]; do
-    ip_two_array+=( "${ip_two%%"$delimiter"*}" );
-    ip_two=${ip_two#*"$delimiter"};
-done;
+      ip_two_array+=("${ip_two%%"$delimiter"*}")
+      ip_two=${ip_two#*"$delimiter"}
+done
 
 ## Convert the IP addresses to unsigned integers
-(( ip_one=(ip_one_array[0] * 16777216) + (ip_one_array[1] * 65536) + (ip_one_array[2] * 256) + (ip_one_array[3]) ));
-(( ip_two=(ip_two_array[0] * 16777216) + (ip_two_array[1] * 65536) + (ip_two_array[2] * 256) + (ip_two_array[3]) ));
+((ip_one = (ip_one_array[0] * 16777216) + (ip_one_array[1] * 65536) + (ip_one_array[2] * 256) + (ip_one_array[3])))
+((ip_two = (ip_two_array[0] * 16777216) + (ip_two_array[1] * 65536) + (ip_two_array[2] * 256) + (ip_two_array[3])))
 
 ## Tell the HostOS to set the DNS servers accordingly
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
-  dbus-send \
-  --system \
-  --reply-timeout=2000 \
-  --print-reply=literal \
-  --type=method_call \
-  --dest=uk.org.thekelleys.dnsmasq \
-  /uk/org/thekelleys/dnsmasq  \
-  uk.org.thekelleys.SetServers \
-  uint32:$ip_one uint32:$ip_two
+      dbus-send \
+      --system \
+      --reply-timeout=2000 \
+      --print-reply=literal \
+      --type=method_call \
+      --dest=uk.org.thekelleys.dnsmasq \
+      /uk/org/thekelleys/dnsmasq \
+      uk.org.thekelleys.SetServers \
+      uint32:$ip_one uint32:$ip_two
